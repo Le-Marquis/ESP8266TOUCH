d6d3c74ceaeab780e1862c3a6292d78af83e8bae
luxcyfer <46075014+luxcyfer@users.noreply.github.com>
2020-03-12 22:18:13 +0100

    Add files via upload

---
+++ sketch_jun29a.ino
@@ -0,0 +1,29 @@
+const int ain=A0;
+const int LED=2;
+int inputVal=0;
+
+void setup()
+{
+  pinMode(LED,OUTPUT); 
+  Serial.begin(9600);
+}
+void loop()
+{
+inputVal=analogRead(ain);
+
+if(inputVal<=20){
+analogWrite(LED,1024);
+}
+else if(inputVal>20 && inputVal<50)analogWrite(LED,768);
+else if(inputVal>50 && inputVal<100)analogWrite(LED,512);
+else if(inputVal>100 && inputVal<1000)analogWrite(LED,256);
+else {analogWrite(LED,0);}
+
+//for serial monitor
+
+for(int i=0;i<inputVal;i++){
+Serial.print("-");
+}
+
+Serial.println(inputVal);
+}
